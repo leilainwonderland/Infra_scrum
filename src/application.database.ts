70cfca5fbde07ee3ca8e5b1beb82ac1a73f7b5f0
leilainwonderland <leila.nunez98@gmail.com>
2022-07-18 14:18:07 +0200

    init database

---
+++ src/application.database.ts
@@ -0,0 +1,22 @@
+import { DataSource } from 'typeorm';
+
+const initDatabase = async () => {
+  const connect = new DataSource({
+    type: 'mariadb',
+    host: process.env.DB_HOST,
+    port: 3306,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_TEST,
+    entities: ['src/models/*.ts'],
+    synchronize: true,
+  });
+  try {
+    await connect.initialize();
+    console.log('Connect to db successfully');
+  } catch (e) {
+    console.error(e);
+  };
+};
+
+export { initDatabase };
