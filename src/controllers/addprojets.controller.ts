00ddc1eb6976ba7270616780efd541c1715c7814
renaud <renaud.hallaert@gmail.com>
2022-07-20 14:30:03 +0200

    addprojects

--- src/controllers/addprojets.controller.ts
+++ src/controllers/addprojets.controller.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from "express";
+import { projectRepository } from "../application.database.js";
 
-const addprojects = (req:Request, res:Response) => {
-    console.log(req.body);
-    res.status(200).json({
-        message: "ok"
-    });
+const addprojects = async (req:Request, res:Response) => {
+    console.log('addprojects');
+    try {
+        const Projects = projectRepository.create(req.body);
+        await projectRepository.save(Projects);
+        res.status(201).json(Projects);
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 export { addprojects };
