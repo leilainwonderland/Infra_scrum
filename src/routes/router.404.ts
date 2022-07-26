f02cc8ea2545cf782e6b4b6a77009180872640ab
Davidg86 <david.gilsoul@hotmail.com>
2022-07-26 09:44:46 +0200

    404 done

--- src/routes/router.404.ts
+++ src/routes/router.404.ts
@@ -1,9 +1,12 @@
+import type { NextFunction, Request, Response } from 'express';
 import { Router } from 'express';
+import { err, ifError } from '../middlewares/error.middleware.js';
 
 const errorRouter: Router = Router();
 
-errorRouter.post('*', (_req, res) => {
-  res.status(404).send('Page inexistante!');
+errorRouter.get('*', (req:Request, res:Response, next: NextFunction) => {
+  ifError('Not Found', 404);
+  return next(err);
 });
 
 export { errorRouter };
