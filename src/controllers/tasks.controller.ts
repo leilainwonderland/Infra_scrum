ff40df3da721fd42f1656aad57504a6fede8bd66
Davidg86 <david.gilsoul@hotmail.com>
2022-07-26 16:51:24 +0200

    delete tasks

--- src/controllers/tasks.controller.ts
+++ src/controllers/tasks.controller.ts
@@ -1,4 +1,4 @@
-import type { Request, Response } from 'express';
+import type { NextFunction, Request, Response } from 'express';
 import { projectRepository, tasksRepository } from '../application.database.js';
 
 const addTasks = async (req: Request, res: Response) => {
@@ -14,17 +14,10 @@ const addTasks = async (req: Request, res: Response) => {
   }
 };
 
-// const deleteTasks = async (req: Request, res: Response, next: NextFunction) => {
-//   const task = await tasksRepository
-//     .createQueryBuilder('task')
-//     .where('task.id = :id', { id: req.body.id })
-//     .leftJoinAndSelect('task.project', 'project')
-//     .getOne();
-//   if (task?.project.id === req.body.projectId) {
-//     await tasksRepository.delete(task!.id);
-//     return res.status(200).json({ status: 'OK' });
-//   }
-//   ifError('Forbidden', 403);
-//   return next(err);
-// };
-export { addTasks };
+const deleteTasks = async (req: Request, res: Response, next: NextFunction) => {
+  console.log('deleteTasks');
+  const task = await tasksRepository.findOneBy({ id: req.body.id });
+  await tasksRepository.delete(task!.id);
+  return res.status(200).json({ status: 'OK' });
+};
+export { addTasks, deleteTasks };
