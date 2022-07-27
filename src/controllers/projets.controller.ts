72d234c6dbd88ec9e56789e22f158303dd34afe0
leilainwonderland <leila.nunez98@gmail.com>
2022-07-27 17:08:13 +0200

    patch for add users

--- src/controllers/projets.controller.ts
+++ src/controllers/projets.controller.ts
@@ -4,6 +4,8 @@ import type { JwtPayload } from 'jsonwebtoken';
 import { decode } from 'jsonwebtoken';
 import { projectRepository, userRepository } from '../application.database.js';
 import { err, ifError } from '../middlewares/error.middleware.js';
+import type { User } from '../models/users.model.js';
+import { Project } from '../models/projects.model.js';
 
 const addprojects = async (req:Request, res:Response) => {
   const token = req.headers.authorization!.split(' ')[1];
@@ -51,47 +53,77 @@ const getProjects = async (req: Request, res:Response) => {
 
 const patchProjects = async (req: Request, res:Response, next:NextFunction) => {
   console.log('patchProjects');
-  // // // On vient chercher l'id de l'utilisateur connecter qui ce trouve dans le token
-  // // const token = req.headers.authorization!.split(' ')[1];
-  // // const userId = await ((decode(token) as JwtPayload).data);
+  /**
+   * VENIR RECHERCHER LES ANCIENS USERS POUR LES AJOUTER À REQ.BODY.USERS !!!
+   */
 
-  // // si req.body.users contient la clef 'users' on vient boucler dedant pour récupérer sa valeur(id)
-  // if (req.body.users) {
-  //   const usersId = req.body.users?.map(async (id: number) => {
-  //     // on vient
-  //     const users = await userRepository
-  //       .createQueryBuilder('users')
-  //       .where('users.id = :id', { id })
-  //       .getOne();
-  //     console.log(users);
-  //     return true;
-  //   });
-  // }
+  // On vient chercher l'id de l'utilisateur connecter qui ce trouve dans le token
+  const token = req.headers.authorization!.split(' ')[1];
+  const userId = await ((decode(token) as JwtPayload).data);
 
-  // const itIsHisProject = await projectRepository
-  //   .createQueryBuilder('project')
-  //   .where('project.id = :id', { id: req.body.id })
-  //   .leftJoinAndSelect('project.userCreator', 'userCreator')
-  //   .leftJoinAndSelect('project.users', 'users')
-  //   .getOne();
+  // si req.body.users existe, on vient boucler dedant pour récupérer les utilisateurs
+  if (req.body.users) {
+    // on y ajoute l'id par defaut du createur du project
+    req.body.users.push(userId);
 
-  // if (itIsHisProject?.userCreator.id === userId) {
-  //   const project = await projectRepository
-  //     .createQueryBuilder()
-  //     .update('project')
-  //     .set(req.body)
-  //     .where('project.id = :id', { id: req.body.id });
-  //   if (Object.keys(req.body).length >= 2) {
-  //     // can still send empty req :( !!
-  //     // await project.execute();
-  //     return res.status(200).json({ status: 'OK' });
-  //   } else {
-  //     ifError('Bad Request', 400);
-  //     return next(err);
-  //   }
-  // }
-  // ifError('Forbidden', 403);
-  // return next(err);
+    const arrayUsers: User[] = [];
+    for (let key of req.body.users) {
+      // key = chaques valeurs contenu dans req.body.user
+      key = await userRepository
+        .createQueryBuilder('users')
+        .where('users.id = :id', { id: key })
+        .getOne();
+
+      // on viens ajouter a arrayUsers les Repository des users
+      arrayUsers.push(key);
+    }
+    // Je ne sais plus, sorry. :(
+    req.body.users = arrayUsers;
+
+    const oldProject = await projectRepository
+      .createQueryBuilder('project')
+      .where('project.id = :id', { id: req.body.id })
+      .leftJoinAndSelect('project.users', 'users')
+      .getOne();
+    try {
+      await projectRepository
+        .createQueryBuilder('project')
+        // la relation que l'on vient update
+        .relation(Project, 'users')
+        // le project que l'on vient update
+        .of(oldProject)
+        // remplace oldProject.users par arrayUsers
+        .addAndRemove(arrayUsers, oldProject?.users);
+      return res.status(201).json({ status: 'OK' });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  if (req.body.users === undefined) {
+    const itIsHisProject = await projectRepository
+      .createQueryBuilder('project')
+      .where('project.id = :id', { id: req.body.id })
+      .leftJoinAndSelect('project.userCreator', 'userCreator')
+      .leftJoinAndSelect('project.users', 'users')
+      .getOne();
+    if (itIsHisProject?.userCreator.id === userId) {
+      const project = await projectRepository
+        .createQueryBuilder()
+        .update('project')
+        .set(req.body)
+        .where('project.id = :id', { id: req.body.id });
+      if (Object.keys(req.body).length >= 2) {
+      // can still send empty req :( !!
+        await project.execute();
+        return res.status(200).json({ status: 'OK' });
+      } else {
+        ifError('Bad Request', 400);
+        return next(err);
+      }
+    }
+  }
+  ifError('Forbidden', 403);
+  return next(err);
 };
 
 export { addprojects, deleteProjects, getProjects, patchProjects };
