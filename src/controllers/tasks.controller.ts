4174ef3879f5ade746e63f81523f450fc3a041dc
renaud <renaud.hallaert@gmail.com>
2022-07-27 11:56:04 +0200

    patchTasks

--- src/controllers/tasks.controller.ts
+++ src/controllers/tasks.controller.ts
@@ -1,5 +1,8 @@
 import type { NextFunction, Request, Response } from 'express';
+import type { JwtPayload } from 'jsonwebtoken';
+import { decode } from 'jsonwebtoken';
 import { projectRepository, tasksRepository } from '../application.database.js';
+import { err, ifError } from '../middlewares/error.middleware.js';
 
 const addTasks = async (req: Request, res: Response) => {
   const projectId = req.body.project;
@@ -20,4 +23,37 @@ const deleteTasks = async (req: Request, res: Response, next: NextFunction) => {
   await tasksRepository.delete(task!.id);
   return res.status(200).json({ status: 'OK' });
 };
-export { addTasks, deleteTasks };
+
+const patchTask = async (req: Request, res: Response, next: NextFunction) => {
+  console.log('patchTask');
+  const token = req.headers.authorization!.split(' ')[1];
+  const userId = await ((decode(token) as JwtPayload).data);
+  const taskByUserInProject = await tasksRepository
+    .createQueryBuilder('task')
+    .where('task.id = :id', { id: req.body.id })
+    .leftJoinAndSelect('task.project', 'project')
+    .getOne();
+  if (taskByUserInProject?.project.userCreator.id === userId) {
+    const task = await tasksRepository
+      .createQueryBuilder()
+      .update('Tasks')
+      .set({
+        name: req.body.name,
+        status: req.body.status,
+        description: req.body.description,
+        priority: req.body.priority,
+        startDate: req.body.startDate,
+        endDate: req.body.endDate,
+        roles: req.body.roles,
+        users: req.body.users,
+      })
+      .where('id = :id', { id: req.body.id })
+      .execute();
+    return res.status(200).json({ status: 'OK' });
+  }
+  ifError('Forbidden', 403);
+  return next(err);
+}
+
+
+export { addTasks, deleteTasks, patchTask };
--- src/routes/router.tasks.ts
+++ src/routes/router.tasks.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { addTasks, deleteTasks } from '../controllers/tasks.controller.js';
+import { addTasks, deleteTasks, patchTask } from '../controllers/tasks.controller.js';
 import { authotized } from '../middlewares/autorized.middleware.js';
 const taskRouter: Router = Router();
 
 taskRouter.post('/:id/new_tasks', authotized, addTasks);
 taskRouter.delete('/:id/delete_tasks', authotized, deleteTasks);
+taskRouter.patch('/:id/patch_tasks', authotized, patchTask);
 
 export { taskRouter };
