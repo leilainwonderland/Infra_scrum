af0f48f597231914970ed7a0f6fd5be5ffe91815
leilainwonderland <leila.nunez98@gmail.com>
2022-07-27 11:53:01 +0200

    thunder test

--- src/routes/router.tasks.ts
+++ src/routes/router.tasks.ts
@@ -3,7 +3,7 @@ import { addTasks, deleteTasks } from '../controllers/tasks.controller.js';
 import { authotized } from '../middlewares/autorized.middleware.js';
 const taskRouter: Router = Router();
 
-taskRouter.post('/:id/new_tasks', authotized, addTasks);
+taskRouter.post('/new_tasks', authotized, addTasks);
 taskRouter.delete('/:id/delete_tasks', authotized, deleteTasks);
 
 export { taskRouter };
