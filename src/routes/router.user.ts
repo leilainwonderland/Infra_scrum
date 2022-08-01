5ea8d934c03b42aadeca661ea83ab02f384e9243
Davidg86 <david.gilsoul@hotmail.com>
2022-08-01 10:30:49 +0200

    !!!

--- src/routes/router.user.ts
+++ src/routes/router.user.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { newUser, getDataUser, login, updateUser, deleteUserForProject, deleteUserForTask, allUsers } from '../controllers/user.controller.js';
+import { newUser, getDataUser, login, updateUser, deleteUserForProject, deleteUserForTask } from '../controllers/user.controller.js';
 import { authorized } from '../middlewares/authorized.middleware.js';
 
 const userRouter: Router = Router();
@@ -7,11 +7,6 @@ const userRouter: Router = Router();
 userRouter.post('/newUser', newUser);
 userRouter.post('/login', login);
 userRouter.get('/userHome', authorized, getDataUser);
-<<<<<<< HEAD
-
-=======
-userRouter.get('/allUsers', authorized, allUsers);
->>>>>>> feature/renaud
 userRouter.patch('/home/updateUser', authorized, updateUser);
 // userRouter.delete('/home/delete_profile', authorized, deleteUser);
 userRouter.delete('/deleteUserForProject/:id', authorized, deleteUserForProject);
